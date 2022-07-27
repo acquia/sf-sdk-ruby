fe694b2d6d1a0670ca70dd48d536120a5f03c13b
Devanshu Mistry <devanshumistry23@gmail.com>
2022-07-27 23:12:25 +0530

    Adding editing stack endpoint to sdk

--- lib/sfrest/codebase.rb
+++ lib/sfrest/codebase.rb
@@ -10,9 +10,23 @@ module SFRest
 
     # Lists the codebases
     # @return [Hash] A hash of codebases configured for the factory.
-    # { "stacks" => { 1 => "abcde", 2 => 'fghij' } }
+    # { "stacks" => { 1 => {"name" : "abcde", "description": "xyz"} , 2 => {"name":"fghij", "description": "xyzsc" } } }
     def list
       @conn.get('/api/v1/stacks')
+    end
+
+    # Edits stack name and description.
+    # @param [Integer] stack_id.
+    # @param [String] name.
+    # @param [String] description.
+    #
+    # @return [Array] an array containing the message given by the server.
+    def edit_stacks(stack_id, name, description)
+      payload = {
+        'name' => name,
+        'description' => description
+      }
+      @conn.put("/api/v1/stacks/#{stack_id}", payload.to_json)
     end
   end
 end
--- spec/codebase_spec.rb
+++ spec/codebase_spec.rb
@@ -20,4 +20,21 @@ describe SFRest::Codebase do
       expect(res['method']).to eq 'get'
     end
   end
+
+  describe '#edit_stack_name_and_description' do
+    id = 1
+    path = "/api/v1/stacks/#{id}"
+
+    it 'calls the stacks put endpoint to edit name and description' do
+      stub_request(:any, /.*#{@mock_endpoint}.*#{path}/)
+        .with(headers: @mock_headers)
+        .to_return { |request| { body: { uri: request.uri, body: request.body, method: request.method }.to_json } }
+      res = @conn.codebase.edit_stacks(id, 'new-name', 'new-description')
+      uri = URI res['uri']
+      expect(uri.path).to eq path
+      expect(res['method']).to eq 'put'
+      expect(JSON(res['body'])['name']).to eq 'new-name'
+      expect(JSON(res['body'])['description']).to eq 'new-description'
+    end
+  end
 end
